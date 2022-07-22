2bddc8cde9f2492c0f7e1b12772223134a8c5eb9
WaterWolf5918 <Lucas117gaming@gmail.com>
2022-07-22 00:37:49 -0400

    .

---
+++ javascript/index.js
@@ -0,0 +1,35 @@
+
+
+
+// function serveripclick(){
+// document.getElementById("main-menu").classList.replace("show", "hide");
+// document.getElementById("server-menu").classList.replace("hide", "show");
+// console.log("serveripclick");
+// }
+
+// function getserverip(ip){
+// document.getElementById("server-menu").classList.replace("show", "hide");
+// document.getElementById("main-menu").classList.replace("hide", "show");
+// CopyToClipboard(ip);
+// console.log("getserverip");
+
+// }
+
+
+// async function CopyToClipboard(id){
+// 	var copyText = document.getElementById(id);
+// 	copyText.select();
+// 	copyText.setSelectionRange(0, 99999);
+// 	navigator.clipboard.writeText(copyText.value);
+// 	console.log("CopyToClipboard");
+// 	document.getElementById("server-ip").classList.replace("hide", "show");
+// 	document.getElementById("server-ip-text").innerText = 'Copyed to clipboard:\n' + copyText.value;
+// 	await new Promise(r => setTimeout(r, 5000));
+// 	document.getElementById("server-ip").classList.replace("show", "hide");
+// }
+
+
+
+
+
+
